1595b9bcaada12a831117d4ec0836781618c7911
sithum vihanga <wwevihanga@gmail.com>
2024-07-08 21:50:50 +0530

    Update App.js

--- crudapp/src/App.js
+++ crudapp/src/App.js
@@ -4,7 +4,15 @@ import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
