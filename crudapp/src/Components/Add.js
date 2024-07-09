040466fffebaef088f9d75330ff45e5acbbde636
sithum vihanga <wwevihanga@gmail.com>
2024-07-09 21:02:16 +0530

    Add.js

---
+++ crudapp/src/Components/Add.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Employees from "./Employees";
+import { v4 as uuid } from "uuid";
+import { Link, useNavigate } from "react-router-dom";
+
+function Add() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  let history = useNavigate();
+
+  return (
+    <div>
+      <Form className="d-grid gap-2" style={{ margin: "15rem" }}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            required
+          ></Form.Control>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
+
+export default Add;
