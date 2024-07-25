698e906629699821c47e63ebe978579a3285de31
sithum vihanga <wwevihanga@gmail.com>
2024-07-25 14:42:11 +0530

    more changes to Edit.js

--- crudapp/src/Components/Edit.js
+++ crudapp/src/Components/Edit.js
@@ -11,6 +11,48 @@ function Edit() {
   const [id, setId] = useState("");
 
   let history = useNavigate();
+
+  var index = Employees.map(function (e) {
+    return e.id;
+  }).indexOf(id);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let a = Employees[index];
+    a.Name = name;
+    a.Age = age;
+
+    history("/");
+  };
+
+  return (
+    <div>
+      <Form className="d-grid gap-2" style={{ margin: "15rem" }}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formAge">
+          <Form.Control
+            type="text"
+            placeholder="Enter Age"
+            value={age}
+            required
+            onChange={(e) => setAge(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button onClick={(e) => handleSubmit(e)} type="submit">
+          Update
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default Edit;
